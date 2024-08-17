43720890d9442409b462925c8f0a5207f4ce1019
Rendell Raven A. Torres <rendelltorres14@gmail.com>
2024-08-17 19:04:15 +0800

    Add files via upload

---
+++ arrays/array.js
@@ -0,0 +1,9 @@
+let names = ['shane', 'rendell', 'ryan', 'hener'];
+console.log(names.length);
+console.log(names[0]);
+console.log(names.length-1);
+
+//updatingArrays
+let friends = ['strange feelings', 'hener'];
+friends[0] = 'shane';
+console.log(friends);
